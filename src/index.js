ccb5f771be5375ae843086fb3e6ab4b358236ab4
abdulhamidnugroho <abdulhamidnugroho@gmail.com>
2020-09-07 00:07:37 +0700

    automatic scrolling

--- src/index.js
+++ src/index.js
@@ -47,7 +47,7 @@ io.on('connection', (socket) => {
         if (filter.isProfane(message)) {
             return callback('Profanity is not allowed')
         }
-  
+        
         io.to(user.room).emit('message', generateMessage(user.username, message))
         callback()
     })
